d818cfe3a064f7eaec51759c136c55b139f3fe65
Misha-Mody <mishamody7@gmail.com>
2022-04-25 22:38:55 -0700

    fix(test): Fix cypress icon test

--- cypress/integration/MainPage.spec.js
+++ cypress/integration/MainPage.spec.js
@@ -7,18 +7,21 @@ describe("renders the main directory page", () => {
 
 describe("icons in the sort of table column are displayed correctly", () => {
   it("display the sort both ways icon initially", () => {
-    cy.get("thead > tr > :nth-child(2) > :nth-child(1)").click();
-    cy.get(".fa-sort").should("exist");
+    cy.visit("/");
+    cy.get(":nth-child(2) > :nth-child(2) > .fa").click({ force: true });
+    cy.get(".fa-sort-asc").should("exist");
   });
-  it("display the sort descending order icon on first click", () => {
-    cy.get("thead > tr > :nth-child(2) > :nth-child(1)").click();
-    cy.get(".fa-sort-desc").should("exist");
+
+  it("display the sort asc icon on first click", () => {
+    cy.visit("/");
+    cy.get(":nth-child(2) > :nth-child(2) > .fa").click({ force: true });
+    cy.get(".fa-sort-asc").should("exist");
+    cy.get(":nth-child(2) > :nth-child(2) > .fa").click({ force: true });
   });
-  it("display the sort ascending order icon on second click", () => {
-    cy.get("thead > tr > :nth-child(2) > :nth-child(1)").click();
-  });
+
   it("again display the sort both ways icon on third click", () => {
-    cy.get("thead > tr > :nth-child(2) > :nth-child(1)").click();
+    cy.visit("/");
+    cy.get(":nth-child(2) > :nth-child(2) > .fa").click({ force: true });
     cy.get(".fa-sort").should("exist");
   });
 });
